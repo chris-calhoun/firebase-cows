b87ae07b0fe557388a4f160140bbc83292714f8c
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-09-19 09:44:33 -0500

    added api keys from firebase.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,9 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 
 const init = () => {
-  console.warn('HELLO!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
 };
 
 init();
